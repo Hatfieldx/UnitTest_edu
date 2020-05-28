89bbdf727873b7949065ef7f2f75065a694ab3eb
Hatfieldx <tsyganov.nn@gmail.com>
2020-05-28 23:00:27 +0700

    [add] init first simple lesson

---
+++ src/Lesson1/Calc.Tests/SolverTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Calc.Tests
+{    
+    public class SolverTests
+    {
+        [Fact]
+        public void Sum_5_4_Res9()
+        {
+            //Arang
+            double x = 5;
+            double y = 4;
+
+            //Act
+
+            double res = Solver.Sum(x, y);
+
+            //Acert
+
+            Assert.Equal(9, res);
+        }
+    }
+}
---
+++ src/Lesson1/Calc/Solver.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Calc
+{
+    public class Solver
+    {
+        public static double Sum(double x, double y) => x + y;
+    }
+}
